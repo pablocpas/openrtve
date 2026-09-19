b4de995ad8cf2b86a4afcaaa987aaec1dbfa3f69
Pablo Caño <pablocpascual@gmail.com>
2026-09-19 23:50:11 +0200

    Versión 0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "es.openrtve"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.1"
+        versionCode = 5
+        versionName = "0.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
